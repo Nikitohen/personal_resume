9b375527faba112ed5bf2d8e8be3575539aec270
Nikita Taraikovics <nikita.taraikovics@gmail.com>
2025-02-11 19:42:46 +0200

    Amend styles and js
    Amended styles to be mobile-first and corrected js for vertical line height.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // h2 and p element variables
 
-const h2Elements = document.querySelectorAll('.about-me .heading, .early-90 .heading, .early-2000 .heading, .mid-2000 .heading, .education .heading, .finance .heading, .manufacturing .heading, .current-time .heading');
+const h2Elements = document.querySelectorAll('.about-me h2, .early-90 h2, .early-2000 h2, .mid-2000 h2, .education h2, .finance h2, .manufacturing h2, .current-time h2');
 const pElements = document.querySelectorAll('.about-me .text-block, .early-90 .text-block, .early-2000 .text-block, .mid-2000 .text-block, .education .text-block, .finance .text-block, .manufacturing .text-block, .current-time .text-block');
 
 // function to toggle visibility of p element on/off and add/remove vertical line respectively
@@ -10,10 +10,10 @@ function toggleText(index) {
 
     if (currentDisplay === 'block') {
         pElements[index].style.display = 'none';
-        currentHeight -= 215;
+        currentHeight -= 245;
     } else {
         pElements[index].style.display = 'block';
-        currentHeight += 215;
+        currentHeight += 245;
     }
 
     verticalLine.style.height = `${currentHeight}px`;
@@ -25,7 +25,6 @@ h2Elements.forEach((h2Element, index) => {
 });
 
 // vertical line variables
-
 const verticalLine = document.querySelector('.vertical-line');
 
-let currentHeight = 1000;
+let currentHeight = 700;
